249cb1098d1581958a8606a469519d07094bb118
Jason Zvaniga <github@kingofalldata.com>
2022-06-19 13:58:58 -0400

    updated the banner to have the domain name.

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -132,13 +132,13 @@ export const directory = async (args?: string[]): Promise<string> => {
 
 export const banner = (args?: string[]): string => {
   return `
-oooo                                  .o8                oooo                  oooo  oooo  
-'888                                 "888                '888                  '888  '888  
- 888  oooo   .ooooo.   .oooo.    .oooo888        .oooo.o  888 .oo.    .ooooo.   888   888  
- 888 .8P'   d88' '88b 'P  )88b  d88' '888       d88(  "8  888P"Y88b  d88' '88b  888   888  
- 888888.    888   888  .oP"888  888   888       '"Y88b.   888   888  888ooo888  888   888  
- 888 '88b.  888   888 d8(  888  888   888       o.  )88b  888   888  888    .o  888   888  
-o888o o888o 'Y8bod8P' 'Y888""8o 'Y8bod88P"      8""888P' o888o o888o 'Y8bod8P' o888o o888o 
+oooo                                  .o8               oooo        
+'888                                 "888               '888        
+ 888  oooo   .ooooo.   .oooo.    .oooo888       .oooo.o  888 .oo.   
+ 888 .8P'   d88' '88b 'P  )88b  d88' '888      d88(  "8  888P"Y88b  
+ 888888.    888   888  .oP"888  888   888      '"Y88b.   888   888  
+ 888 '88b.  888   888 d8(  888  888   888  .o. o.  )88b  888   888  
+o888o o888o 'Y8bod8P' 'Y888""8o 'Y8bod88P" Y8P 8""888P' o888o o888o 
 ..    .-.. --- ...- .    -.-- --- ..- 
 
 Type 'help' to see the list of commands.
