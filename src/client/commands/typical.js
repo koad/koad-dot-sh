ca798fefcb274da12547bd4ba945bca27cf7264c
koad on wonderland <git@koad.sh>
2022-09-10 18:48:17 -0400

    added 'll' command alias

--- src/client/commands/typical.js
+++ src/client/commands/typical.js
@@ -14,6 +14,17 @@ drwxrwxr-x  19 guest        guest           4096 May 30 13:19  .koad-io/
 `;
 };
 
+commands["ll"] = function(args){
+    return `total 220
+drwxr-xr-x 131 guest        guest           4096 Jun 17 13:22  .
+drwxr-xr-x   3 root         root            4096 Mar 17  2021  ..
+drwxrwxr-x  26 guest        guest           4096 Jun 10 15:49  .alice/
+drwxrwxr-x  24 guest        guest           4096 Jun  8 16:08  .astro/
+drwxrwxr-x  17 guest        guest           4096 May 28 22:06  .bob/
+drwxrwxr-x  19 guest        guest           4096 May 30 13:19  .koad-io/
+`;
+};
+
 commands["help"] = function(args){
   console.log(args)
   if(args && args[0] == '--all') {
