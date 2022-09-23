a49358356f137ecfb3c14635e0e2dddad3785320
koad on wonderland <git@koad.sh>
2022-09-23 16:49:30 -0400

    Moved rick to a dedicated commands file.

--- src/client/commands/links.js
+++ src/client/commands/links.js
@@ -2,61 +2,56 @@
 
 
 commands["directory"] = function(args){
-  window.open('https://canadaecoin.directory', 'indieexpress'); // ...I'm sorry
+  window.open('https://canadaecoin.directory', 'indieexpress'); 
   return `opened indie express via example 'canadaecoin.directory' in browser window`;
 };
 
 commands["disco"] = function(args){
-  window.open('https://player.koad.sh', 'koadplayer'); // ...I'm sorry
+  window.open('https://player.koad.sh', 'koadplayer'); 
   return `opened 'player.koad.sh' in browser window`;
 };
 
 commands["discord"] = function(args){
-  window.open('https://discord.gg/Qnhwtqmcb2', '_blank'); // ...I'm sorry
+  window.open('https://discord.gg/Qnhwtqmcb2', '_blank'); 
   return `opened invite to koad's discord channel in browser window `;
 };
 
 commands["docs"] = function(args){
-  window.open('https://book.koad.sh', 'koadiodocs'); // ...I'm sorry
+  window.open('https://book.koad.sh', 'koadiodocs'); 
   return `opened 'book.koad.sh' in browser window`;
 };
 
 commands["github"] = function(args){
-  window.open('https://github.com/koad', 'github'); // ...I'm sorry
+  window.open('https://github.com/koad', 'github'); 
   return `opened 'github.com/koad' in browser window`;
 };
 
 commands["keybase"] = function(args){
-  window.open('https://keybase.io/koad', 'koadlive'); // ...I'm sorry
+  window.open('https://keybase.io/koad', 'koadlive'); 
   return `opened 'keybase.io/koad' in browser window`;
 };
 
 commands["live"] = function(args){
-  window.open('https://live.koad.sh', 'koadlive'); // ...I'm sorry
+  window.open('https://live.koad.sh', 'koadlive'); 
   return `opened 'live.koad.sh' in browser window`;
 };
 
 commands["matrix"] = function(args){
-  window.open('https://matrix.to/#/#live:koad.sh', '_blank'); // ...I'm sorry
+  window.open('https://matrix.to/#/#live:koad.sh', '_blank'); 
   return `opened invite to koad's live matrix space in browser window `;
 };
 
-commands["onlyfans"] = function(args){
-  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
-  return `opened 'onlyfans' in browser window, lol.`;
+commands["scan"] = function(args){
+  window.open('https://scan.koad.sh', 'scanner'); 
+  return `opened 'scan.koad.sh' in browser window`;
 };
 
 commands["status"] = function(args){
-  window.open('https://status.koad.sh', 'statusdashboard'); // ...I'm sorry
+  window.open('https://status.koad.sh', 'statusdashboard'); 
   return `opened 'status.koad.sh' in browser window`;
 };
 
-commands["sudo"] = function(args){
-  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
-  return `Permission denied: with little power comes... no responsibility? `;
-};
-
 commands["wallet"] = function(args){
-  window.open('https://ecoincore.com', 'ecoincore'); // ...I'm sorry
+  window.open('https://ecoincore.com', 'ecoincore'); 
   return `opened 'ecoincore.com' in browser window`;
 };
---
+++ src/client/commands/rickrolls.js
@@ -0,0 +1,11 @@
+// ...I'm sorry,... not.
+
+commands["onlyfans"] = function(args){
+  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+  return `opened 'onlyfans' in browser window, lol.`;
+};
+
+commands["sudo"] = function(args){
+  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+  return `Permission denied: with little power comes... no responsibility. `;
+};
